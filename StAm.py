9ba3e7d2b9dd7f301eebc37e615e76b3fff4382e
KajoDeza <diellzakajoshaj@gmail.com>
2025-02-12 21:58:33 +0100

    Add files via upload

---
+++ StAm.py
@@ -0,0 +1,17 @@
+states_of_america = ["Delaware", "Pennsylvania", "New Jersey", "Georgia"]
+
+print(states_of_america [-3])
+
+states_of_america.append("Connecticut")
+print(states_of_america)
+
+states_of_america[1] = "Pencilvania"
+print(states_of_america)
+
+
+
+
+
+
+
+
